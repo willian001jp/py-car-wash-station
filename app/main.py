fe09b485f9054f24d36fd5c3b2265d48412622f1
Willian Nomiyama <willian001@gmail.com>
2025-03-31 17:06:48 -0300

    Add for in serve_cars, wash_single annotations

--- app/main.py
+++ app/main.py
@@ -20,16 +20,17 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, *args: Car) -> float:
-        if args.clean_mark < self.clean_power:
-            rent += self.calculate_washing_price()
-            return rent
+    def serve_cars(self, cars: list[Car]) -> float:
+        rent = 0
+        for car in cars:
+            if car.clean_mark < self.clean_power:
+                rent += self.calculate_washing_price(Car)
+        return rent
 
     def calculate_washing_price(self, car: Car) -> float:
-        return (car.comfort_class *
-                 (self.clean_power - car.clean_mark) * self.average_rating) / self.distance_from_city_center
+        return ((car.comfort_class * (self.clean_power - car.clean_mark) * self.average_rating) / self.distance_from_city_center)
 
-    def wash_single_car():
+    def wash_single_car(self, car: Car):
         pass
 
     def rate_service(self) -> None:
